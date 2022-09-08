865c1c683df6b55bf1a96348c95ab8fedffe37ae
jonahchoi <cjonah227@gmail.com>
2022-09-08 12:34:29 -0700

    es6 stack added

--- sprint-one/src/es6/stack.js
+++ sprint-one/src/es6/stack.js
@@ -2,6 +2,24 @@ class Stack {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.idx = 0;
+  }
+
+  push(value) {
+    this.storage[this.idx++] = value;
+  }
+
+  pop() {
+    if(this.idx !== 0) {
+      var result = this.storage[--this.idx];
+      this.storage[this.idx] = undefined;
+      return result;
+    }
+  }
+
+  size() {
+    return this.idx;
   }
 
 }
