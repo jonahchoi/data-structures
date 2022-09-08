58d0f665d3e3a7d513d89057041daea905c3810b
jonahchoi <cjonah227@gmail.com>
2022-09-08 12:35:48 -0700

    es6 queue added

--- sprint-one/src/es6/queue.js
+++ sprint-one/src/es6/queue.js
@@ -2,6 +2,25 @@ class Queue {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
   constructor() {
+    this.storage = {};
+    this.storeIdx = 0;
+    this.retreiveIdx = 0;
+  }
+
+  enqueue(value) {
+    this.storage[this.storeIdx++] = value;
+  }
+
+  dequeue() {
+    if(this.storeIdx !== this.retreiveIdx) {
+      var result = this.storage[this.retreiveIdx];
+      this.storage[this.retreiveIdx++] = undefined;
+      return result;
+    }
+  }
+
+  size() {
+    return this.storeIdx - this.retreiveIdx;
   }
 
 }
