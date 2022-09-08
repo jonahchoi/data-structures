11cf39fc01ccddd4787162ae6849122bde7dfa80
jonahchoi <cjonah227@gmail.com>
2022-09-08 16:42:32 -0700

    tree complete

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -3,17 +3,41 @@ var Tree = function(value) {
   newTree.value = value;
 
   // your code here
-  newTree.children = null;  // fix me
-
+  // children will be an empty array upon instantiation
+  newTree.children = [];  // fix me
+  newTree = _.extend(newTree, treeMethods);
   return newTree;
 };
 
 var treeMethods = {};
 
 treeMethods.addChild = function(value) {
+  // create new tree and pass in value
+  var childTree = Tree(value);
+  // push new tree to tree.children
+  this.children.push(childTree);
 };
 
 treeMethods.contains = function(target) {
+  // if target matches tree
+  if (this.value === target) {
+    // return true
+    return true;
+  }
+
+  var result = false;
+  // main recursive case
+  // loop through tree children
+  this.children.forEach(function(child) {
+    // return result of contains for each child
+    if(!result) {
+      result = child.contains(target);
+    }
+  })
+
+  // base case
+  // if no children return false
+  return result;
 };
 
 
