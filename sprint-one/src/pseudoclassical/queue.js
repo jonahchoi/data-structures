6f1f98f4a2dd296c2e7f5320e009ff74264c4019
jonahchoi <cjonah227@gmail.com>
2022-09-08 12:30:28 -0700

    pseudoclassical queue added

--- sprint-one/src/pseudoclassical/queue.js
+++ sprint-one/src/pseudoclassical/queue.js
@@ -1,6 +1,25 @@
 var Queue = function() {
   // Hey! Rewrite in the new style. Your code will wind up looking very similar,
   // but try not not reference your old code in writing the new style.
+  this.storage = {};
+  this.storeIdx = 0;
+  this.retreiveIdx = 0;
+};
+
+Queue.prototype.enqueue = function(value) {
+  this.storage[this.storeIdx++] = value;
+};
+
+Queue.prototype.dequeue = function() {
+  if(this.storeIdx !== this.retreiveIdx) {
+    var result = this.storage[this.retreiveIdx];
+    this.storage[this.retreiveIdx++] = undefined;
+    return result;
+  }
+};
+
+Queue.prototype.size = function() {
+  return this.storeIdx - this.retreiveIdx;
 };
 
 
