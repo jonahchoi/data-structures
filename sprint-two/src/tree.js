86bd016144ac9168792a9187e6a8787e6ef0dca5
jonahchoi <cjonah227@gmail.com>
2022-09-10 11:59:49 -0700

    tree parent functionality added

--- sprint-two/src/tree.js
+++ sprint-two/src/tree.js
@@ -1,8 +1,8 @@
 var Tree = function(value) {
   var newTree = {};
   newTree.value = value;
+  newTree.parent = null;
 
-  // your code here
   // children will be an empty array upon instantiation
   newTree.children = [];  // fix me
   newTree = _.extend(newTree, treeMethods);
@@ -11,11 +11,24 @@ var Tree = function(value) {
 
 var treeMethods = {};
 
+// A .removeFromParent() method, which disassociates the tree with its parent (in both directions)
+treeMethods.removeFromParent = function() {
+  var currentTree = this;
+  //This parent children,
+  this.parent.children = this.parent.children.filter(function(child) {
+    return (child !== currentTree) ? true : false;
+  })
+  //set current parent to null
+  this.parent = null;
+}
+
 treeMethods.addChild = function(value) {
   // create new tree and pass in value
   var childTree = Tree(value);
+  childTree.parent = this;
   // push new tree to tree.children
   this.children.push(childTree);
+
 };
 
 treeMethods.contains = function(target) {
@@ -40,8 +53,21 @@ treeMethods.contains = function(target) {
   return result;
 };
 
+var treeTester = function() {
+  var testTree = Tree(1);
 
+  testTree.addChild(2);
+  testTree.addChild(3);
 
+  console.log(testTree.contains(3));
+
+  testTree.children[1].removeFromParent();
+
+  console.log(testTree.children);
+
+}
+
+// treeTester();
 /*
  * Complexity: What is the time complexity of the above functions?
  */
