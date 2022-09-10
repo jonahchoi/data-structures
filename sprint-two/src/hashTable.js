69d4e451e3aa07c0c277a2c21955b5994bdc9bb6
jonahchoi <cjonah227@gmail.com>
2022-09-10 14:42:55 -0700

    rehashing hashTable complete

--- sprint-two/src/hashTable.js
+++ sprint-two/src/hashTable.js
@@ -3,14 +3,57 @@
 var HashTable = function() {
   this._limit = 8;
   this._storage = LimitedArray(this._limit);
+  this._length = 0;
+
+
 };
 
-HashTable.prototype.insert = function(k, v) {
+HashTable.prototype.checkLength = function() {
+  var runNewStorage = false;
+
+  //If length greater than 75% of limit
+  if((this._length / this._limit) > 0.75) {
+    this._limit *= 2;
+    runNewStorage = true;
+  }
+  //If length less than 25%
+  else if((this._length / this._limit) < 0.25) {
+    this._limit /= 2;
+    runNewStorage = true;
+  }
+
+  if(runNewStorage) {
+    // console.log(this._limit)
+    var newStorage = LimitedArray(this._limit);
+    var currentObject = this;
+    //hash old array into neww array
+    this._storage.each(function(bucket) {
+      if(bucket) {
+        for (let j = 0; j < bucket.length; j++) {
+          currentObject.insert(bucket[j][0], bucket[j][1], newStorage);
+        }
+      }
+    })
+    this._storage = newStorage;
+  }
+}
+//storage: [ arrays[ tuple[0,1], tuple[0,1]], array[], array[]]
+
+HashTable.prototype.insert = function(k, v, store) {
   var index = getIndexBelowMaxForKey(k, this._limit);
+  var dontReHash = false;
+  if(!store){
+    store = this._storage;
+    dontReHash = true;
+  }
   //set limited array at index with value
-  var bucket = this._storage.get(index) || [];
+  var bucket = store.get(index) || [];
   bucket.push([k, v]);
-  this._storage.set(index, bucket);
+  store.set(index, bucket);
+  if(dontReHash) {
+    this._length++;
+    this.checkLength();
+  }
 };
 
 HashTable.prototype.retrieve = function(k) {
@@ -37,7 +80,25 @@ HashTable.prototype.remove = function(k) {
       bucket.splice(i, 1);
     }
   });
+  this._length--;
+  this.checkLength();
 };
+
+var hashTester = function() {
+  var testHash = new HashTable();
+
+  testHash.insert('abc', '1');
+  testHash.insert('def', '2');
+  testHash.insert('ghi', '3');
+  testHash.insert('jkl', '4');
+  testHash.remove('abc');
+  testHash.remove('def');
+  testHash.remove('ghi');
+  console.log(testHash);
+
+}
+
+// hashTester();
 
 /*
  * Complexity: What is the time complexity of the above functions?
